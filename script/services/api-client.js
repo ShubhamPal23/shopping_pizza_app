b476599529d3623d95242572fe36b4ad624d6a20
SHUBHAM PAL <140602488+ShubhamPal23@users.noreply.github.com>
2023-12-25 00:01:58 +0530

    Update api-client.js

--- script/services/api-client.js
+++ script/services/api-client.js
@@ -1,29 +1,26 @@
-import URL from '../utils/constant.js';
-// asnc code behave as await
+//https call
+import URL from '../Utils/constant.js';
 async function NetworkCall(){
-    
     try{
-    const response=await fetch(URL);
-    const data=await response.json();//synchronous blocked
-    return data;
+        const response =  await fetch(URL);           // same code as below to avoid  callback hell
+        const object = await response.json();
+        return object;
     }
     catch(err){
-        console.log('Some problem in API call',err);
+        console.log("error in API call");
         throw err;
     }
 }
-// function makeNetworkCall(){
-//     const promise=fetch(URL);
-//     console.log('Promise is :',promise);
-//     promise.then(response=>{
-//         console.log('Response is : ',response);
-//         const promise2=response.json();
-//         promise2.then(data=>console.log('Data is: ',data)).catch(e=>console.log('JSON is giving parse '))
-//     }).catch(err=>console.log('ERROR'))
-// }
 export default NetworkCall;
-//three states of fetch : 1.pending 2.Fulfilled 3.Rejected
-//SYNC : 1. CPU CENTRIC 2. call->wait->return/result 3. BLOCKING 
-//ASYNC : 1. I/O CENTRIC 2. NON BLOCKING 3.
 
-//Event queue; 
+// function networkcall(){
+//     const promise = fetch(URL);
+//     console.log("promise is", promise)
+//     promise.then(response=>{
+//         console.log("response",response)
+//         const prmoise2=response.json();  //json to object                               
+//         prmoise2.then(data=>console.log).catch(e=>console.log("json parse error"))
+//     }).catch(e=>{
+//         console.log("error unable to fetch")
+//     })
+// }
