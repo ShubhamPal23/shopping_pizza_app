36e85f07c70123e156cb1a6ec32efa0ba3f61b4c
SHUBHAM PAL <140602488+ShubhamPal23@users.noreply.github.com>
2023-12-24 23:59:11 +0530

    Update product-operations.js

--- script/services/product-operations.js
+++ script/services/product-operations.js
@@ -1,46 +1,32 @@
-// Contains the Logic for Fetching ,
-// Adding, Sorting, Searching,
- // Deletion , Updation
- /*
-  It talk to Network Layer to Bring the JSON, and
-  convert JSON into Objects vice-versa
+//contain code  for fethching insertion , deletion etc
+import Product from '../model/product.js';
+import NetworkCall from './api-client.js';
+const productOperations = {
+    products:[], // Key:value
+    search(pizzaId,val){
+        const product = this.products.find(currentProduct=>currentProduct.id==pizzaId);
+        console.log('Product Found ', product);
+        if (val==="+"){product.quantity=product.quantity+1;}
+        else{product.quantity=product.quantity-1;}
+        console.log('Array ', this.products);
+    },
+    getProductsInCart(){
+        const productInBasket = this.products.filter(product=>product.quantity);
+        return productInBasket;
+    },
 
- */
-  import Product from '../models/product.js';
-  import NetworkCall from './api-client.js';
-  
-  const productOperations = {
-      products:[], // Key:value
-      update_quantity(pizzaId,val){
-          const product = this.products.find(currentProduct=>currentProduct.id==pizzaId);
-          console.log('Product Found ', product);
-          if(val==='+')product.quantity =product.quantity+1;
-          else product.quantity =product.quantity-1;
-          console.log('Array ', this.products);
-      },
-      getProductsInCart(){
-          const productInBasket = this.products.filter(product=>product.quantity);
-          return productInBasket;
-      },
-  
-      async loadProducts(){
-          const pizzas = await NetworkCall();
-          console.log("pizzas : ",pizzas);
-          const pizzaArray = pizzas['Vegetarian'];
-          const productsArray = pizzaArray.map(pizza=>{
-              const currentPizza = new Product(pizza.id, pizza.name,
-                   pizza.menu_description, pizza.price, pizza.assets.product_details_page[0].url);
-                  return currentPizza;
-                  })
-                  console.log('********Product Array ', productsArray);
-                  this.products = productsArray;
-                  return productsArray;  // Wrap in Promise
-              },
-      sortProducts(){
-  
-      },
-      searchProducts(){
-  
-      }
-  }
-  export default productOperations;
+    async loadProducts(){
+        const pizzas = await NetworkCall();
+        const pizzaArray = pizzas['Vegetarian'];
+        const productsArray = pizzaArray.map(pizza=>{
+            const currentPizza = new Product(pizza.id, pizza.name,
+                 pizza.menu_description, pizza.price, pizza.assets.product_details_page[0].url);
+                return currentPizza;
+                })
+                console.log('********Product Array ', productsArray);
+                this.products = productsArray;
+                return productsArray;  // Wrap in Promise
+            },
+    
+}
+export default productOperations;
